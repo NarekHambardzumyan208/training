24ae4374401278c069762f18a72d019e9571bf0b
anerkaner10@mail.ru <anerkaner10@mail.ru>
2023-06-30 09:31:38 +0400

    Edited

--- UpgradedAnimal.cpp
+++ UpgradedAnimal.cpp
@@ -5,31 +5,44 @@ protected:
     int age = 0;
     std::string name;
 public:
-    Animal() {}
-    Animal(bool hungry, std::string name, int age) : hungry(hungry), name(name), age(age) {}
-    virtual void sound() {
+    Animal(){}
+    Animal(bool hungry,std::string name,int age): hungry(hungry), name(name),age(age){}
+    virtual void sound(){
         std::cout << "Animal sound\n";
     };
+    void operator()() {
+        std::cout << "Im a Functor\n";
+        std::cout << "This is the name of animal\n" << name;
+    }
+    bool operator<(const Animal&obj) {
+        return age < obj.age;
+    }
+    bool operator>(const Animal& obj) {
+        return age > obj.age;
+    }
+    bool operator==(const Animal& obj) {
+        return age == obj.age;
+    }
 };
 class Mammal :virtual protected Animal {
 public:
     Mammal() {}
-    Mammal(std::string mammalname, bool hungry, int age) :Animal(hungry, mammalname, age) {}
-    void sound() {
+    Mammal(std::string mammalname, bool hungry,int age) :Animal(hungry,mammalname,age){}
+    void sound(){
         std::cout << "Mammal sound\n";
     }
 };
 class Fish :virtual protected Animal {
 public:
-    Fish() {}
-    Fish(std::string FishName, bool hungry, int age) : Animal(hungry, FishName, age) {}
+    Fish(){}
+    Fish(std::string FishName,bool hungry,int age) : Animal(hungry, FishName, age){}
     void sound() {
         std::cout << "Fish sound\n";
     }
 };
 class Frog :virtual protected Fish,virtual protected Mammal {
 public:
-    Frog(std::string FrogName, bool hungry, int age) : Animal(hungry, FrogName, age) {}
+    Frog(std::string FrogName, bool hungry, int age) : Animal(hungry,FrogName,age){}
     void sound() {
         std::cout << "grr,grr\n";
     }
@@ -37,16 +50,23 @@ public:
         std::cout << "age\n" << age;
     };
 };
-class Lion : protected Mammal {
+class Lion: protected Mammal {
 public:
-    Lion(std::string LionName, bool hungry, int age) : Mammal(LionName, hungry, age) {}
+    Lion(std::string LionName,bool hungry,int age) : Mammal(LionName,hungry,age){}
     void sound() final
-    {
+    { 
         std::cout << "Roarr!!!!\n";
     }
 };
 int main()
 {
+    Animal Kiosa(1, "Armenian", 5);
+    Kiosa();
+    Animal Rebato(1, "American", 25);
+    std::cout << "Is Kiosa obj younger Rebato obj2\n" << (Kiosa < Rebato) << "\n";
+    std::cout << "Is Kiosa obj older Rebato obj2\n" << (Kiosa > Rebato) << "\n";
+    std::cout << "Is Kiosa obj = Rebato obj2 age\n" << (Kiosa == Rebato) << "\n";
+
     Lion Armenian("Caucauss", 1, 15);
     Armenian.sound();
     Frog Caucauss("Armenian", 1, 25);
